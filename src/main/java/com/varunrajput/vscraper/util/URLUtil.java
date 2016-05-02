f57fcbb18bda8ba2cf2066a94abea28b8b94647c
Varun Rajput <varun_sf@hotmail.com>
2016-05-01 19:27:43 -0700

    Switching to http-components from commons-client

--- src/main/java/com/varunrajput/vscraper/util/URLUtil.java
+++ src/main/java/com/varunrajput/vscraper/util/URLUtil.java
@@ -1,27 +1,25 @@
 package com.varunrajput.vscraper.util;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
-import org.apache.commons.httpclient.HostConfiguration;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
-import org.apache.commons.httpclient.params.HttpMethodParams;
+import com.varunrajput.vscraper.Property;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.varunrajput.vscraper.Property;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
 
 /**
  * @author varunrajput
@@ -30,87 +28,88 @@ public class URLUtil {
   private static final Logger log = LoggerFactory.getLogger(URLUtil.class);
   private static Integer numRetries = Integer.parseInt(PropertiesUtil
       .get(Property.NumRetries));
-  
-  private static HttpClient client = null;
-  
+
+  private static HttpClient httpClient;
+
   static {
-    int numScrapers = Integer.parseInt(PropertiesUtil.get(Property.NumScrapers, "100"));
-    MultiThreadedHttpConnectionManager multiThreadedHttpConnectionManager = new MultiThreadedHttpConnectionManager();
-    HttpConnectionManagerParams params = new HttpConnectionManagerParams();
-    params.setMaxConnectionsPerHost(HostConfiguration.ANY_HOST_CONFIGURATION, numScrapers);
-    params.setDefaultMaxConnectionsPerHost(numScrapers);
-    params.setMaxTotalConnections(numScrapers);
-    multiThreadedHttpConnectionManager.setParams(params);
-    client = new HttpClient(multiThreadedHttpConnectionManager);
+    int numScrapers = Integer.parseInt(PropertiesUtil.get(Property.NumScrapers));
+    int numRoutes = PropertiesUtil.get(Property.BaseUrl).split(",").length;
+
+    PoolingHttpClientConnectionManager cnMgr = new PoolingHttpClientConnectionManager();
+    cnMgr.setMaxTotal(numScrapers);
+    cnMgr.setDefaultMaxPerRoute(numScrapers/numRoutes);
+
+    httpClient = HttpClientBuilder.create()
+        .setRetryHandler(new DefaultHttpRequestRetryHandler(numRetries, false))
+        .setMaxConnTotal(numScrapers)
+        .setMaxConnPerRoute(numScrapers/numRoutes)
+        .setConnectionManager(cnMgr)
+        .build();
   }
-  
+
   public static String getOutputString(String baseUrl,
-      Map<String,String> params, QueryMethod queryMethod) {
-    
+                                       Map<String,String> params, QueryMethod queryMethod) {
+
     String outputString = null;
-    HttpMethod method = generateMethod(baseUrl, params, queryMethod);
-    
+
     try {
-      int statusCode = client.executeMethod(method);
-      if (statusCode == HttpStatus.SC_OK) {
-        InputStream is = method.getResponseBodyAsStream();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        StringBuilder builder = new StringBuilder();
-        String line = "";
-        while ((line = rd.readLine()) != null) {
-          builder.append(line);
+      HttpResponse httpResponse = httpClient.execute(generateMethod(baseUrl, params, queryMethod));
+
+      if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+          sb.append(line);
         }
-        outputString = builder.toString();
-        rd.close();
+        outputString = sb.toString();
+        reader.close();
+
       } else {
-        log.error("NON OK Status Code Received: " + statusCode + ". Reason: "
-            + method.getStatusText());
+        log.error("NON OK Status Code Received: " + httpResponse.getStatusLine().getStatusCode()
+            + ". Reason: " + httpResponse.getStatusLine().getReasonPhrase());
       }
-    } catch (Exception e) {
+
+    } catch (IOException e) {
       log.error(e.getMessage(), e);
-    } finally {
-      if (method != null) {
-        method.releaseConnection();
-      }
     }
-    
+
     return outputString;
   }
-  
-  private static HttpMethod generateMethod(String baseUrl,
-      Map<String,String> params, QueryMethod queryMethod) {
-    
-    HttpMethod method = null;
+
+  private static HttpUriRequest generateMethod(String baseUrl,
+                                               Map<String,String> params, QueryMethod queryMethod) {
+
+    URI uri;
+    try {
+      URIBuilder uriBuilder = new URIBuilder(baseUrl);
+      for (Map.Entry<String, String> entry : params.entrySet()) {
+        uriBuilder.addParameter(entry.getKey(), entry.getValue());
+      }
+      uri = uriBuilder.build();
+    }catch (URISyntaxException e) {
+      log.error("Invalid base URL '" + baseUrl + "' " + e.getMessage(), e);
+      return null;
+    }
+
+    HttpUriRequest request;
     switch (queryMethod) {
       case GET:
-        method = new GetMethod(baseUrl);
+        request = new HttpGet(uri);
         break;
       case POST:
-        method = new PostMethod(baseUrl);
+        request = new HttpPost(uri);
         break;
       default:
-        method = new GetMethod(baseUrl);
+        request = new HttpGet(uri);
         break;
     }
-    
-    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-    for (String param : params.keySet()) {
-      nameValuePairs.add(new NameValuePair(param, params.get(param)));
-    }
-    
-    method.setQueryString(nameValuePairs
-        .toArray(new NameValuePair[nameValuePairs.size()]));
-    
-    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
-        new DefaultHttpMethodRetryHandler(numRetries, false));
-    
-    method.getParams().setParameter(HttpMethodParams.SO_TIMEOUT,
-        Integer.parseInt(PropertiesUtil.get(Property.SocketTimeout)));
-    
-    return method;
+
+    return request;
   }
-  
-  public static enum QueryMethod {
+
+  public enum QueryMethod {
     GET, POST
   }
 }
